e7c56e4054f66b72083820a7812c378b80fef64c
Your Name <you@example.com>
2022-11-17 19:22:25 -0600

    added window event

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function(){
+    console.log('window loaded')
+});
+
+takeoff.addEventListener('click', function(){
+const readyForTakeoff=window.confirm('Confirm that the shuttle is ready for takeoff.');
+if(readyForTakeoff===true);
+flightStatus
+})
